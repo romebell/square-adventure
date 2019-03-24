5966e37416a5c70f5b080a25758b746793b2c6ea
Rome Bell <romebell@me.com>
2019-03-24 15:09:03 -0700

    Add #step for each time user presses buttons and #handleInput for handling inputs

--- app.js
+++ app.js
@@ -8,7 +8,36 @@ let player_y = 20;
 let base_x = Math.floor(Math.random() * 300);
 let base_y = Math.floor(Math.random() * 300);
 
-ctx.fillStyle = 'red';
-ctx.fillRect(player_x, player_y, 15, 15); // ctx.fillRect(x, y, width, height)
 
-ctx.strokeRect(base_x, base_y, 50, 50);
+
+function step() {
+  ctx.clearRect(0, 0, 350, 350); // clears the screen on each iteration of the step
+  ctx.fillStyle = 'red';
+  ctx.fillRect(player_x, player_y, 15, 15); // ctx.fillRect(x, y, width, height)
+  ctx.strokeRect(base_x, base_y, 50, 50);
+  requestAnimationFrame(step);
+}
+
+requestAnimationFrame(step);
+
+function handleInput(event){
+  const key = event.key;
+  switch(key){
+    case 'a':
+      player_x -= 5;
+      break;
+      case 'w':
+      player_y -= 5;
+      break;
+      case 'd':
+      player_x += 5;
+      break;
+      case 's': 
+      player_y += 5;
+      break;
+    default:
+      return;
+  }
+}
+
+document.addEventListener('keypress', handleInput);
