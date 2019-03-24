c92753b102c6617ba9ae499b7639af2946876f30
Rome Bell <romebell@me.com>
2019-03-24 14:42:43 -0700

    Add players variables and base with random position

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const canvas = document.querySelector('canvas');
 canvas.width = 350;
 canvas.height = 350;
+
+const ctx = canvas.getContext('2d');
+let player_x = 20;
+let player_y = 20;
+let base_x = Math.floor(Math.random() * 300);
+let base_y = Math.floor(Math.random() * 300);
+
+ctx.fillStyle = 'red';
+ctx.fillRect(player_x, player_y, 15, 15); // ctx.fillRect(x, y, width, height)
+
+ctx.strokeRect(base_x, base_y, 50, 50);
