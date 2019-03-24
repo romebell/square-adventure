bcf3e41d3680689e7e6c4365d04b2ac14934a772
Rome Bell <romebell@me.com>
2019-03-24 15:34:24 -0700

    Add score to game feature and add conditional to move target to random place on board

--- app.js
+++ app.js
@@ -8,13 +8,26 @@ let player_y = 20;
 let base_x = Math.floor(Math.random() * 300);
 let base_y = Math.floor(Math.random() * 300);
 
+let score = document.querySelector('span');
+let value = Number(score.innerHTML);
+
 
 
 function step() {
   ctx.clearRect(0, 0, 350, 350); // clears the screen on each iteration of the step
   ctx.fillStyle = 'red';
+  // player_x += 1; // experiment where player_x runs across the screen
   ctx.fillRect(player_x, player_y, 15, 15); // ctx.fillRect(x, y, width, height)
   ctx.strokeRect(base_x, base_y, 50, 50);
+
+  if (player_x >= base_x && player_y >= base_y && 
+    player_x <= base_x + 50 && player_y <= base_y + 50){
+      base_x = Math.floor(Math.random() * 300);
+      base_y = Math.floor(Math.random() * 300);
+      value++;
+      score.textContent = `${value}`; // change the score
+  }
+
   requestAnimationFrame(step);
 }
 
